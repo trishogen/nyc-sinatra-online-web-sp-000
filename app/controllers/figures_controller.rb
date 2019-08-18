ff81ea02ec06914a072a6d892d19f257991c44ff
Trish Ogen <patriciaogen@gmail.com>
2019-08-18 19:17:33 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -41,7 +41,7 @@ class FiguresController < ApplicationController
     figure = Figure.find(params[:id])
 
     if !params[:figure][:name].empty?
-      figure.update(name: params[:figure][:name])
+      figure.update(params[:figure])
     end
 
     if !params[:title][:name].empty?
