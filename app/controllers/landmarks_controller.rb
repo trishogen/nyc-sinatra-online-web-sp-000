841ba030f3abd42d383aed2361dc2566542a5622
Trish Ogen <patriciaogen@gmail.com>
2019-08-18 19:49:25 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,16 +1,16 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmarks.all
+    erb :'landmarks/index'
+  end
+  
   get '/landmarks/new' do
     erb :'landmarks/new'
   end
 
   post '/landmarks' do
     landmark = Landmark.create(params[:landmark])
-  end
-
-  get '/landmarks' do
-    @landmarks = Landmarks.all
-    erb :'landmarks/index'
   end
 
 end
