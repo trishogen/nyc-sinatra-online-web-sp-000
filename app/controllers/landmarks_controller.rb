65602d37763eef09b1cf0001f09374dd360de11d
Trish Ogen <patriciaogen@gmail.com>
2019-08-18 19:50:09 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,10 +1,10 @@
 class LandmarksController < ApplicationController
 
   get '/landmarks' do
-    @landmarks = Landmarks.all
+    @landmarks = Landmark.all
     erb :'landmarks/index'
   end
-  
+
   get '/landmarks/new' do
     erb :'landmarks/new'
   end
