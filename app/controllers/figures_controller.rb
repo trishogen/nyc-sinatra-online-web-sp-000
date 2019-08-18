ea893de464b4b8a60af67e97020eeb926554790d
Trish Ogen <patriciaogen@gmail.com>
2019-08-18 18:50:26 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -18,6 +18,8 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/edit'
   end
 
